b2f87657995a94a164e7f6038d13d3449ba02183
Nothim205 <Najhimg@gmail.com>
2023-05-26 15:26:30 -0700

    added movement

---
+++ JustTheTwoUs/Assets/Scripts/PLayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayerController : MonoBehaviour
+{
+    public float _speed = 1;
+    public Rigidbody _rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+     void Update()
+    {
+        var dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        transform.Translate(dir * _speed * Time.deltaTime);
+
+        _rb.velocity = dir * _speed;
+        if (Input.GetKeyDown(KeyCode.W)) transform.position += Vector3.forward;
+    }
+}
