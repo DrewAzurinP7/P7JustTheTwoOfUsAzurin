3b625be1734997267911d7c495922b56012014ca
Ak1 <116124126+DrewAzurinP7@users.noreply.github.com>
2023-05-31 12:13:56 -0700

    Added shooting script
    Added shooting script

--- JustTheTwoUs/Assets/Scripts/PLayerController.cs
+++ JustTheTwoUs/Assets/Scripts/PLayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PLayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float _speed = 1;
     public Rigidbody _rb;
